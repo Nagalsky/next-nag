849ed0a78f26ab2810e50a4eadf06f93bdd705d1
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2022-11-17 16:41:11 +0100

    added tailwind

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,10 +15,20 @@ export default function Header() {
       {session ? (
         <>
           <p>Welcome, {session?.user?.email}</p>
-          <button onClick={() => signOut()}>log out</button>
+          <button
+            onClick={() => signOut()}
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  focus:outline-none"
+          >
+            log out
+          </button>
         </>
       ) : (
-        <button onClick={() => signIn()}>sign in</button>
+        <button
+          onClick={() => signIn()}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  focus:outline-none"
+        >
+          sign in
+        </button>
       )}
     </header>
   )
