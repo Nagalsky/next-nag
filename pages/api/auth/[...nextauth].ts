35b61a3cbef6eacb100e071890c0a06d6174a010
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2022-11-28 10:12:35 +0100

    dwdwwd

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,6 +11,10 @@ export const authOptions: NextAuthOptions = {
   theme: {
     colorScheme: "light",
   },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60 // 30 days
+  },
   pages: {
     signIn: '/auth/signin'
   }
