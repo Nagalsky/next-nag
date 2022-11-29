810a0b65c920140424b4fe968f66eeb33be5fdf4
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2022-11-29 12:43:39 +0100

    rebuild page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,9 @@
-import { useSelector } from 'react-redux'
-import Counter from '../components/counter'
-import { RootState } from '../store/store'
-
 export default function IndexPage() {
-  const { count } = useSelector((state: RootState) => state.counter)
   return (
     <>
       <section className="py-6">
         <div className="container">
           <h1 className="font-bold text-red-500">NextAuth.js Example</h1>
-          <h2>Count: {count}</h2>
-
-          <Counter />
         </div>
       </section>
     </>
--- pages/me.tsx
+++ pages/me.tsx
@@ -1,12 +1,21 @@
 import { useSession } from 'next-auth/react'
+import { useSelector } from 'react-redux'
+import Counter from '../components/counter'
+import { RootState } from '../store/store'
 
 export default function MePage() {
   const { data } = useSession()
+
+  const { count } = useSelector((state: RootState) => state.counter)
 
   return (
     <>
       <section className="py-6">
         <div className="container">
+          <h2>Count: {count}</h2>
+
+          <Counter />
+
           <div className="break-all">
             <pre className="whitespace-pre-wrap">
               {JSON.stringify(data, null, 2)}
